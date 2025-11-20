5bfe6cac5ee88422cfbf86da6811c57328fd91fe
shirin <sparchekani@gmail.com>
2025-11-20 16:45:31 +0100

    empliment diet endpoint

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-def sumnum(a: int, b: int) -> int:
-    result = a+b
-    return result
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
 
 
-def mulnum(a: int, b: int) -> int:
-    result = a*b
-    return result
+class Person(BaseModel):
+    weight: float
+    height: float
+
+
+@app.post("/diet")
+def diet_recommendation(person: Person):
+    height_m = person.height/100
+    bmi = person.weight/(height_m**2)
+    if bmi > 24.9:
+        diet = "Cut"
+    elif bmi < 18.5:
+        diet = "Bulk"
+    else:
+        diet = "maintain"
+
+    return {"bmi": round(bmi, 2), "diet": diet}
