2197408dd64195632fdea0857f3aa486ac644b02
shirin <sparchekani@gmail.com>
2025-11-20 17:33:31 +0100

    impliment calories endpoint

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from fastapi import FastAPI
 from pydantic import BaseModel
 app = FastAPI()
@@ -8,15 +9,41 @@ class Person(BaseModel):
     height: float
 
 
+class Person_info(BaseModel):
+    height: float
+    weight: float
+    age: int
+    sex: Literal["male", "female"]
+
+
 @app.post("/diet")
-def diet_recommendation(person: Person):
+def diet_recommendation(person: Person_info):
     height_m = person.height/100
     bmi = person.weight/(height_m**2)
+    bmr = calc_bmr(person)
     if bmi > 24.9:
-        diet = "Cut"
+        calories = bmr - 500
+        diet = "cut"
     elif bmi < 18.5:
-        diet = "Bulk"
+        calories = bmr + 500
+        diet = "bulk"
     else:
+        calories = bmr
         diet = "maintain"
 
-    return {"bmi": round(bmi, 2), "diet": diet}
+    return {"bmi": round(bmi, 2), "diet": diet, "calorie": calories}
+
+
+@app.post("/claories")
+def calorie_recommendation(person_info: Person_info):
+    bmr = calc_bmr(person_info)
+
+    return round(bmr)
+
+
+def calc_bmr(person_info: Person_info):
+    if person_info.sex == "male":
+        bmr = 10*person_info.weight + 6.25*person_info.height - 5*person_info.age + 5
+    else:
+        bmr = 10*person_info.weight + 6.25*person_info.height - 5*person_info.age - 161
+    return bmr
